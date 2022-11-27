db571b278d7a2ee4400c5096de8610705a953fd7
duygukucukoglu35 <duygukucukoglu35@gmail.com>
2022-11-27 10:54:21 +0300

    make a consume reducer redux store

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,12 +1,12 @@
 //kural 1:initial state
-const initialState={
+export const initialState={
     counter:0
 }
  
 //kural 2:reducer oluşturulur
 //action={type,payload}
 //const[counter,setCounter]=useState()
-const reducer=(state=initialState,{type})=>{
+export const reducer=(state=initialState,{type})=>{
     switch (type) {
         case "ARTTIR":
             return{counter: state.counter +1}
