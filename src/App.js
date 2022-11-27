f180e43e175140ecfb7317a18a050f55e5f61abd
duygukucukoglu35 <duygukucukoglu35@gmail.com>
2022-11-27 10:37:35 +0300

    done create reducer and state function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import Counter from "./components/Counter";
+import { legacy_createStore as createStore} from 'redux';
+import {Provider} from "react-redux";
+import{reducer} from "./redux";
 
 
 function App() {
+  const store = createStore(reducer)
   return (
     <div>
-    <Counter/>
+      <Provider store={store}>
+      <Counter/>
+      </Provider>
+    
       
     </div>
   );
 }
 
 export default App;
+
+//store kurma adımları
+//1.createStore() | redux
+//2.provider() | react-redux
+//3.consume
